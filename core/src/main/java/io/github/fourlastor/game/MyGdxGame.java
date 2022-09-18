a9cbeb286cb8857af04655f71aecfebc677b0a9d
Daniele Conti <fourlastor@users.noreply.github.com>
2022-09-18 16:44:27 +0100

    Use provider instead of lazy

--- core/src/main/java/io/github/fourlastor/game/MyGdxGame.java
+++ core/src/main/java/io/github/fourlastor/game/MyGdxGame.java
@@ -1,15 +1,15 @@
 package io.github.fourlastor.game;
 
 import com.badlogic.gdx.Game;
-import dagger.Lazy;
 import io.github.fourlastor.game.di.GameComponent;
 import io.github.fourlastor.game.gameplay.LevelScreen;
+import javax.inject.Provider;
 
 public class MyGdxGame extends Game {
 
-    private final Lazy<LevelScreen> screen;
+    private final Provider<LevelScreen> screen;
 
-    public MyGdxGame(Lazy<LevelScreen> screen) {
+    public MyGdxGame(Provider<LevelScreen> screen) {
         this.screen = screen;
     }
 
--- core/src/main/java/io/github/fourlastor/game/gameplay/LevelComponent.java
+++ core/src/main/java/io/github/fourlastor/game/gameplay/LevelComponent.java
@@ -1,15 +1,15 @@
 package io.github.fourlastor.game.gameplay;
 
-import dagger.Lazy;
 import dagger.Subcomponent;
 import io.github.fourlastor.game.di.ScreenScoped;
+import javax.inject.Provider;
 
 @ScreenScoped
 @Subcomponent(modules = LevelModule.class)
 public interface LevelComponent {
 
     @ScreenScoped
-    Lazy<LevelScreen> levelScreen();
+    Provider<LevelScreen> levelScreen();
 
     @Subcomponent.Builder
     interface Builder {
