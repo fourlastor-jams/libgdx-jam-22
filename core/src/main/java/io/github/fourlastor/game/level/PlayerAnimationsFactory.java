358911ea44f4bb2ad075ecde6d9787cdd231e9f8
Daniele Conti <fourlastor@users.noreply.github.com>
2022-09-20 12:39:05 +0100

    Display jumping animation

--- core/src/main/java/io/github/fourlastor/game/level/PlayerAnimationsFactory.java
+++ core/src/main/java/io/github/fourlastor/game/level/PlayerAnimationsFactory.java
@@ -13,18 +13,26 @@ public class PlayerAnimationsFactory {
 
     public static final String ANIMATION_STANDING = "player/OnGround/OnGround";
     public static final String ANIMATION_FALLING = "player/Falling/Falling";
+    public static final String ANIMATION_JUMPING = "player/Jumping/Jumping";
 
     @Provides
     @ScreenScoped
     @Named(ANIMATION_STANDING)
-    public Animation<TextureRegion> provideStanding(TextureAtlas textureAtlas) {
+    public Animation<TextureRegion> standing(TextureAtlas textureAtlas) {
         return new Animation<>(0.2f, textureAtlas.findRegions(ANIMATION_STANDING), Animation.PlayMode.LOOP);
     }
 
     @Provides
     @ScreenScoped
     @Named(ANIMATION_FALLING)
-    public Animation<TextureRegion> provideFalling(TextureAtlas textureAtlas) {
+    public Animation<TextureRegion> falling(TextureAtlas textureAtlas) {
         return new Animation<>(0.1f, textureAtlas.findRegions(ANIMATION_FALLING), Animation.PlayMode.NORMAL);
+    }
+
+    @Provides
+    @ScreenScoped
+    @Named(ANIMATION_JUMPING)
+    public Animation<TextureRegion> jumping(TextureAtlas textureAtlas) {
+        return new Animation<>(0.4f, textureAtlas.findRegions(ANIMATION_JUMPING), Animation.PlayMode.NORMAL);
     }
 }
